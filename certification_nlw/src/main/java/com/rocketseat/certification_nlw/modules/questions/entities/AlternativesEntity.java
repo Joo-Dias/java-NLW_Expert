7213c3ad9235e4d690a02a31a95bfc5afbfbc7f3
João Dias <joaovitorelf@gmail.com>
2024-02-07 17:50:00 -0300

    aula 02

--- certification_nlw/src/main/java/com/rocketseat/certification_nlw/modules/questions/entities/AlternativesEntity.java
+++ certification_nlw/src/main/java/com/rocketseat/certification_nlw/modules/questions/entities/AlternativesEntity.java
@@ -1,30 +1,30 @@
-package com.rocketseat.certification_nlw.modules.students.entities;
+package com.rocketseat.certification_nlw.modules.questions.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
+@Entity(name = "alternatives")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "students")
-public class StudentEntity {
+public class AlternativesEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(unique = true, nullable = false)
-    private String email;
+    private String description;
 
-    @OneToMany(mappedBy = "studentEntity")
-    private List<CertificationStudentEntity> certificationStudentEntity;
+    private boolean isCorrect;
 
     @CreationTimestamp
     private LocalDate createdAt;
--- certification_nlw/src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ certification_nlw/src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,19 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    CertificationStudentRepository repository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
-        if(dto.getEmail().equals("joaodias@gmail.com") && dto.getTechnology().equals("JAVA")){
+        var result = this.repository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if(!result.isEmpty()){
             return true;
         }
         return false;
